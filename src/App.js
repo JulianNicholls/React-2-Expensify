315f69b75cc7ccae07b94154310e4935e0690a07
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-26 20:36:36 +0100

    Add react-router-dom module and set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const ExpenseDashboardPage = () => <h1>Expensify Dashboard</h1>;
+
+const AddExpensePage = () => <h1>Add Expense</h1>;
+
+const EditExpensePage = () => <h1>Edit Expense</h1>;
+
+const HelpPage = () => <h1>Help!</h1>;
 
 class ExpensifyApp extends React.Component {
   render() {
     return (
-      <React.Fragment>
-      <header>
-        <h1>Expensify App</h1>
-      </header>
-      <p>Expensification</p>
-      </React.Fragment>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={ExpenseDashboardPage} />
+          <Route path="/create" component={AddExpensePage} />
+          <Route path="/edit" component={EditExpensePage} />
+          <Route path="/help" component={HelpPage} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
