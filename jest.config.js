a1e94d86340ca49c48e59e6cb62bb44950c80e07
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-30 13:48:20 +0100

    Install and configure Jest and Enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  testURL: 'http://localhost:8080'
+  testURL: 'http://localhost:8080',
+  setupFiles: ['raf/polyfill', '<rootDir>/src/tests/setupTests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
