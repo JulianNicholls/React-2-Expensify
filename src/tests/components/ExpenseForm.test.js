3e0b295fa5a268cb11f79e4ab863ecd7a8e31794
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-30 15:13:11 +0100

    Test ExpenseForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -16,4 +16,56 @@ describe('ExpenseForm component', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should show an error when invalid data is submitted', () => {
+    const wrapper = shallow(<ExpenseForm />);
+
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state when description is changed', () => {
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper
+      .find('input')
+      .at(0)
+      .simulate('change', { target: { value: 'rent' } });
+
+    expect(wrapper.state('description')).toBe('rent');
+  });
+
+  it('should update state when note is changed', () => {
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find('textarea').simulate('change', { target: { value: 'new note' } });
+
+    expect(wrapper.state('note')).toBe('new note');
+  });
+
+  it('should update state when amount is changed validly', () => {
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper
+      .find('input')
+      .at(1)
+      .simulate('change', { target: { value: '23.50' } });
+
+    expect(wrapper.state('amount')).toBe('23.50');
+  });
+
+  it('should NOT update state when amount is changed INvalidly', () => {
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper
+      .find('input')
+      .at(1)
+      .simulate('change', { target: { value: '12.122' } });
+
+    expect(wrapper.state('amount')).toBe('');
+  });
 });
