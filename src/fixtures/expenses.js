a710190449ce744a89d7237ba9f4cf7792532f98
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-30 11:00:45 +0100

    Move testExpenses to a separate file

---
+++ src/fixtures/expenses.js
@@ -0,0 +1,18 @@
+export const testExpenses = [
+  { id: '1', description: 'rent', amount: 10000, createdAt: 2000000000, note: '' },
+  {
+    id: '2',
+    description: 'rates',
+    amount: 15000,
+    createdAt: 1800000000,
+    note: ''
+  },
+  {
+    id: '3',
+    description: 'water',
+    amount: 20000,
+    createdAt: 1600000000,
+    note: ''
+  },
+  { id: '4', description: 'gas', amount: 25000, createdAt: 1400000000, note: '' }
+];
