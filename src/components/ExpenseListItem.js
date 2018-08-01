df9fd122957f26347be13d0f27eb6ff117d52c85
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-01 20:41:35 +0100

    Install numeral module and format currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
+import 'numeral/locales/en-gb';
+
+numeral.locale('en-gb');
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div style={{ display: 'flex', alignItems: 'center', marginBottom: '7px' }}>
@@ -8,7 +12,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h4 style={{ margin: '0' }}>{description}</h4>
     </Link>
     <div style={{ width: '15%', textAlign: 'right' }}>
-      {(amount / 100.0).toFixed(2)}
+      {numeral(amount / 100.0).format('$0,0.00')}
     </div>
     <div style={{ width: '15%', paddingLeft: '2em' }}>
       {moment(createdAt).format('ll')}
--- src/index.js
+++ src/index.js
@@ -13,21 +13,21 @@ import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
-// Initialise Moment with the correct locale (for me)
-moment.locale('en-gb');
-
-const store = configureStore();
-
 //---------------------------------------------------------------------------
 // TEST CODE
 
 import { addExpense } from './actions/expenses';
 
+// Initialise Moment with the correct locale (for me)
+moment.locale('en-gb');
+
+const store = configureStore();
+
 store.dispatch(
   addExpense({
     description: 'Water bill',
     amount: 10800,
-    createdAt: moment().subtract(27, 'days')
+    createdAt: moment().subtract(20, 'days')
   })
 );
 store.dispatch(
@@ -49,6 +49,14 @@ store.dispatch(
     description: 'Telephone bill',
     amount: 5000,
     createdAt: moment().subtract(5, 'days')
+  })
+);
+
+store.dispatch(
+  addExpense({
+    description: 'Large bill',
+    amount: 1202700,
+    createdAt: moment().subtract(17, 'days')
   })
 );
 
