07ae25f2b1337b594693314bd34402af712aaa6d
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-08 10:52:09 +0100

    Add display name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 
 import { startLogout } from '../actions/auth';
 
-export const Header = ({ startLogout }) => (
+export const Header = ({ displayName, startLogout }) => (
   <header
     style={{
       background: '#202050',
@@ -15,23 +15,37 @@ export const Header = ({ startLogout }) => (
     <div className="container">
       <h1 style={{ margin: '0' }}>Expensify</h1>
 
-      <div style={{ display: 'flex', justifyItems: 'left', marginTop: '1em' }} />
-      <NavLink className="nav-link" to="/dashboard">
-        Dashboard
-      </NavLink>
-      <NavLink className="nav-link" to="/create">
-        Add Expense
-      </NavLink>
-      <button onClick={startLogout}>Log out</button>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'left',
+          alignItems: 'center',
+          marginTop: '1em'
+        }}
+      >
+        <NavLink className="nav-link" to="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink className="nav-link" to="/create">
+          Add Expense
+        </NavLink>
+        <div style={{ marginLeft: 'auto' }}>
+          {displayName} <button onClick={startLogout}>Log out</button>
+        </div>
+      </div>
     </div>
   </header>
 );
+
+const mapStateToProps = state => ({
+  displayName: state.auth.displayName
+});
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({ startLogout }, dispatch);
 };
 
 export default connect(
-  undefined,
+  mapStateToProps,
   mapDispatchToProps
 )(Header);
