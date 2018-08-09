ad46b84082c9c97085e5ee287a465e5be204b68d
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-09 10:10:05 +0100

    Style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ displayName, startLogout }) => (
-  <header
-    style={{
-      background: '#202050',
-      color: '#eee'
-    }}
-  >
-    <div className="container">
-      <h1 style={{ margin: '0' }}>Expensify</h1>
+  <header>
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Expensify</h1>
+        </Link>
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'left',
-          alignItems: 'center',
-          marginTop: '1em'
-        }}
-      >
-        <NavLink className="nav-link" to="/dashboard">
-          Dashboard
-        </NavLink>
-        <NavLink className="nav-link" to="/create">
-          Add Expense
-        </NavLink>
         <div style={{ marginLeft: 'auto' }}>
           {displayName} <button onClick={startLogout}>Log out</button>
         </div>
